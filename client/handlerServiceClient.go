be20dcc262f6b86c4971234fd3b52a9320ab6887
mhchen0219 <37766785+mhchen0219@users.noreply.github.com>
2020-02-26 22:53:20 +0800

    Update handlerServiceClient.go

--- client/handlerServiceClient.go
+++ client/handlerServiceClient.go
@@ -3,8 +3,8 @@ package client
 import (
 	"context"
 	"fmt"
-	"github.com/rico93/v2ray-sspanel-v3-mod_Uim-plugin/model"
-	"github.com/rico93/v2ray-sspanel-v3-mod_Uim-plugin/utility"
+	"github.com/mhchen0219/v2ray-sspanel-v3-mod_Uim-plugin/tree/master/model"
+	"github.com/mhchen0219/v2ray-sspanel-v3-mod_Uim-plugin/tree/master/utility"
 	"google.golang.org/grpc"
 	"strings"
 	"v2ray.com/core"
